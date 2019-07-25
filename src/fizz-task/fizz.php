88f73fa843e0eb7dc70eea6c850910101e7043a2
slavahokage <v_smirnov@ikantam.com>
2019-07-25 12:42:38 +0300

    refactoring fizz task

--- src/fizz-task/fizz.php
+++ src/fizz-task/fizz.php
@@ -4,17 +4,17 @@ namespace PHPTasks\FizzTask;
 
 function displayNumbers(int $begin, int $end)
 {
-    $output = '';
+    $output = [];
+
     for ($i = $begin; $i <= $end; $i++) {
         $isFizz = $i % 3 == 0;
         $isBuzz = $i % 5 == 0;
 
         $fizz = $isFizz ? 'Fizz' : '';
         $buzz = $isBuzz ? 'Buzz' : '';
-        $number = ("$fizz$buzz") ? '' : $i;
 
-        $output .= "$fizz$buzz$number ";
+        ($fizz === '' && $buzz === '') ? $output[] = $i : $output[] = "$fizz$buzz";;
     }
 
-    return $output;
+    return join(' ', $output);
 }
